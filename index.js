8d43049e2735d5abd4bf63cd054042e7ae36d558
eltoncezar <eltoncezar@gmail.com>
2022-01-02 15:24:37 -0300

    New features

--- index.js
+++ index.js
@@ -13,23 +13,37 @@ var argv = require('yargs/yargs')(process.argv.slice(2))
     })
     .example('$0 example.txt', 'default params')
     .example('$0 example.txt -lines 2 -font Bebas -theme dogs', 'custom params')
+    
     .alias('l', 'lines')
     .nargs('l', 1)
     .default('l', 2)
     .describe('l', 'Number of lines to render in each slide')
+    
+    .alias('o', 'opacity')
+    .nargs('o', 1)
+    .default('o', 30)
+    .describe('o', 'Font background transparency, between 0 and 100')
+
+    .alias('c', 'color')
+    .nargs('c', 1)
+    .default('c', 'FFFFFF')
+    .describe('c', 'Font color, hexadecimal value')
+    
     .alias('f', 'font')
     .nargs('f', 1)
     .default('f', 'Arial')
     .describe('f', 'Text font family')
+    
     .alias('k', 'keywords')
     .nargs('k', 1)
     .default('k', ['landscape'])
-    .describe('k', 'The Unsplash keywords that will be used to fetch the backgrounds')
+    .describe('k', 'The Unsplash keywords that will be used to fetch the backgrounds (if you don\'t want backgrounds, use \'none\')')
+    
     .alias('t', 'title')
     .nargs('t', 1)
     .default('t', '')
     .describe('t', 'The presentation title. If not informed, the first line of the txt file will be used.')
-    
+
     // .demandOption(['f'])
     .help('h')
     .alias('h', 'help')
@@ -45,8 +59,8 @@ const readline = require('readline');
 var dir = './temp';
 
 function main() {
-    
-    if (!fs.existsSync(dir)){
+
+    if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
 
@@ -73,11 +87,10 @@ function main() {
 
             var increment = argv.l;
             var slideText = lines[i];
-            if (i < lines.length - argv.l/2)
-            {
+            if (i < lines.length - argv.l / 2) {
                 for (let j = 1; j < argv.l; j++) {
-                    if (!lines[i+j]) { 
-                        increment = j; 
+                    if (!lines[i + j]) {
+                        increment = j;
                         break;
                     }
                     slideText = slideText + '\n' + lines[i + j];
@@ -91,47 +104,47 @@ function main() {
         }
 
         for (var i = 0; i < slides.length; i++) {
-            request('https://source.unsplash.com/featured/1600x900/?'+ argv.k +'?sig' + i).pipe(fs.createWriteStream(dir + '/img' + i + '.png'), {flags: 'w'}).on('close', function() {
-                completedSlides++;
-                console.log("slide " + completedSlides);
-                if (completedSlides == slides.length) writeFile(slides);
-            });
+            request('https://source.unsplash.com/featured/1600x900/?' + argv.k + '?sig' + i)
+                .pipe(fs.createWriteStream(dir + '/img' + i + '.png'), {
+                    flags: 'w'
+                })
+                .on('close', function () {
+                    completedSlides++;
+                    console.log("slide " + completedSlides);
+                    if (completedSlides == slides.length) writeFile(slides);
+                });
         }
     });
 }
 
 function addNewSlide(text, imagePath) {
-
     const slide = pptx.addNewSlide();
 
-    slide.color = '000000';
-    slide.background = {
-        path: imagePath
-    };
-
-    slide.addImage({
-        path: imagePath,
-        w: '100%',
-        h: '100%',
-        x: 0,
-        y: 0
-    });
+    if (argv.k != 'none') {
+        slide.addImage({
+            path: imagePath,
+            w: '100%',
+            h: '100%',
+            x: 0,
+            y: 0
+        });
+    }
 
     slide.addText(text, {
         fontFace: argv.f,
         fontSize: 28,
         bold: true,
-        color: 'FFFFFF',
+        color: argv.c,
         shape: pptx.shapes.RECTANGLE,
         align: 'c',
         x: '0%',
-        y: ((100-(10 * argv.l))/2) + '%',
+        y: ((100 - (10 * argv.l)) / 2) + '%',
         w: '100%',
         h: 10 * argv.l + '%',
         fill: {
             type: 'solid',
             color: '333333',
-            alpha: 30
+            alpha: argv.o
         },
     });
 }
@@ -143,7 +156,10 @@ function writeFile(slides) {
     }
 
     pptx.save(title);
-    fs.rmdirSync(dir, { recursive: true, force: true });
+    fs.rmSync(dir, {
+        recursive: true,
+        force: true
+    });
     console.log('done');
 }
 
